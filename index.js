d343594318e417e7ed843c264a1e34b0fc6e5a3a
victoria <victorialslocum@gmail.com>
2021-08-15 19:02:21 -0700

    it works!

--- index.js
+++ index.js
@@ -15,6 +15,8 @@ exports.getDatabase = async function () {
       task: page.properties.Task.title[0]?.plain_text,
       completed: page.properties.Completed.checkbox,
       category: page.properties.Category.select.name,
+      enddate: page.properties.Due.date,
+      // epic: page.properties. ref: https://stackoverflow.com/questions/67913461/notion-querying-databases-and-pages-provide-limited-properties
     };
   });
 
--- public/main.js
+++ public/main.js
@@ -7,7 +7,8 @@ const getDataFromBackend = async () => {
     console.log(data)
     return data;
 };
-  
+
+
 const doEverything = async () => {
 
     const database = await getDataFromBackend()
@@ -15,80 +16,176 @@ const doEverything = async () => {
     console.log(database)
 
     anychart.onDocumentReady(function () {
+            
+        var sortDatabase = [];
+
+        database.forEach((element) => {
+            var makeKey = element.category;
+            if(!sortDatabase[makeKey]) {
+            sortDatabase[makeKey] = [];
+            }
+        
+            sortDatabase[makeKey].push({
+                task: element.task,
+                category: element.category,
+                completed: element.completed,
+                enddate: element.enddate,
+            });
+            });
+
+        console.log(sortDatabase);
+        console.log(sortDatabase.School);
+
+        const keys = Object.keys(sortDatabase)
+
+
+        keys.forEach((key) => {
+            let items = sortDatabase[key]
+            var data = [];
+            let chartTitle = key + " Notion Tasks"
+
+            console.log(items)
+
+            items.forEach((item) => {
+                if (item.completed == true) {
+                    data.push({x: item.task, value: 1, fill: "#069CCD"})
+                } else {
+                    data.push({x: item.task, value: 1, fill: "#7BB9E7"})
+                }
+        })
+        
+        var chart = anychart.pie();
+    
+        var legend = chart.legend();
+        legend.enabled(true);
+        legend.align("center")
+        legend.position("right")
+        legend.itemsLayout("vertical")
+    
+        // set the chart title
+        chart.title(chartTitle);
+    
+        // add the data
+        chart.data(data);
+    
+        // create container
+        const chartContainer = document.createElement("div")
+        chartContainer.classList.add("container")
+        container.appendChild(chartContainer)   
+
+        // display the chart in the container
+        chart.container(chartContainer);
+        chart.draw();
+
+    });
+            // https://stackoverflow.com/questions/40774697/how-to-group-an-array-of-objects-by-key
+
+            // const groupCategory = _.groupBy(item, item.category));
+            // console.log(groupCategory.get("School"));
+
+            // item.forEach((category) => {
+            //     console.log(item)
+            //     var data = [];
+
+            //     if (item.completed == true) {
+            //         data.push({x: item.task, value: 1, fill: "#069CCD"})
+            //     } else {
+            //         data.push({x: item.task, value: 1, fill: "#7BB9E7"})
+            //     }
+                
+            //     var chart = anychart.pie();
+        
+            //     chart.legend(false);
+            
+            //     // set the chart title
+            //     chart.title("Daily Notion tasks");
+            
+            //     // add the data
+            //     chart.data(data);
+            
+            //     // create container
+            //     const chartContainer = document.createElement("div")
+            //     chartContainer.classList.add("container")
+            //     container.appendChild(chartContainer)   
+
+            //     // display the chart in the container
+            //     chart.container(chartContainer);
+            //     chart.draw();
+            // })
 
         // set the data
-        var dataPersonal = [];
-        var dataDaily = [];
-        var dataSchool = [];
+    //     var dataPersonal = [];
+    //     var dataDaily = [];
+    //     var dataSchool = [];
     
-        database.forEach((item) => {
-            if (item.category == "Daily") {
-                if (item.completed == true) {
-                dataDaily.push({x: item.task, value: 1, fill: "#069CCD"})
-                } else {
-                dataDaily.push({x: item.task, value: 1, fill: "#7BB9E7"})
-                }
-            } else if (item.category == "School") {
-                if (item.completed == true) {
-                dataSchool.push({x: item.task, value: 1, fill: "#069CCD"})
-                } else {
-                dataSchool.push({x: item.task, value: 1, fill: "#7BB9E7"})
-                }
-            } else if (item.category == "Personal") {
-                if (item.completed == true) {
-                dataPersonal.push({x: item.task, value: 1, fill: "#069CCD"})
-                } else {
-                dataPersonal.push({x: item.task, value: 1, fill: "#7BB9E7"})
-                }
-            }
-        })
+    //     database.forEach((item) => {
+    //         if (item.category == "Daily") {
+    //             if (item.completed == true) {
+    //             dataDaily.push({x: item.task, value: 1, fill: "#069CCD"})
+    //             } else {
+    //             dataDaily.push({x: item.task, value: 1, fill: "#7BB9E7"})
+    //             }
+    //         } else if (item.category == "School") {
+    //             if (item.completed == true) {
+    //             dataSchool.push({x: item.task, value: 1, fill: "#069CCD"})
+    //             } else {
+    //             dataSchool.push({x: item.task, value: 1, fill: "#7BB9E7"})
+    //             }
+    //         } else if (item.category == "Personal") {
+    //             if (item.completed == true) {
+    //             dataPersonal.push({x: item.task, value: 1, fill: "#069CCD"})
+    //             } else {
+    //             dataPersonal.push({x: item.task, value: 1, fill: "#7BB9E7"})
+    //             }
+    //         }
+    //     })
     
-        // create the chart
-        var chartDaily = anychart.pie();
+    //     // create the chart
+    //     var chartDaily = anychart.pie();
         
-        chartDaily.legend(false);
+    //     chartDaily.legend(false);
     
-        // set the chart title
-        chartDaily.title("Daily Notion tasks");
+    //     // set the chart title
+    //     chartDaily.title("Daily Notion tasks");
     
-        // add the data
-        chartDaily.data(dataDaily);
+    //     // add the data
+    //     chartDaily.data(dataDaily);
     
-        // display the chart in the container
-        chartDaily.container(container1);
-        chartDaily.draw();
+    //     // display the chart in the container
+    //     chartDaily.container(container1);
+    //     chartDaily.draw();
 
 
-        // create the chart
-        var chartSchool = anychart.pie();
+    //     // create the chart
+    //     var chartSchool = anychart.pie();
     
-        chartSchool.legend(false);
+    //     chartSchool.legend(false);
 
-        // set the chart title
-        chartSchool.title("School Notion tasks");
+    //     // set the chart title
+    //     chartSchool.title("School Notion tasks");
     
-        // add the data
-        chartSchool.data(dataSchool);
+    //     // add the data
+    //     chartSchool.data(dataSchool);
     
-        // display the chart in the container
-        chartSchool.container(container2);
-        chartSchool.draw();
+    //     // display the chart in the container
+    //     chartSchool.container(container2);
+    //     chartSchool.draw();
 
 
-        // create the chart
-        var chartPersonal = anychart.pie();
+    //     // create the chart
+    //     var chartPersonal = anychart.pie();
     
-        chartPersonal.legend(false);
+    //     chartPersonal.legend(false);
 
-        // set the chart title
-        chartPersonal.title("Personal Notion tasks");
+    //     // set the chart title
+    //     chartPersonal.title("Personal Notion tasks");
     
-        // add the data
-        chartPersonal.data(dataPersonal);
+    //     // add the data
+    //     chartPersonal.data(dataPersonal);
     
-        // display the chart in the container
-        chartPersonal.container(container3);
-        chartPersonal.draw();
+    //     // display the chart in the container
+    //     chartPersonal.container(container3);
+    //     chartPersonal.draw();
     
     });
  
