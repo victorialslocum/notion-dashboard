200cd83d130c35c4eaae3ff4e0297c6c90b6882f
victoria <victorialslocum@gmail.com>
2021-08-20 21:44:51 -0700

    confusion

--- public/main.js
+++ public/main.js
@@ -7,6 +7,8 @@ const getDataFromBackend = async () => {
   return data;
 };
 
+// HOW DOES GITHUB WORK
+
 var today = new Date();
 var dd = String(today.getDate()).padStart(2, "0");
 var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
