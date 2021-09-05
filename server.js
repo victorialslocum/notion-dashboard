dc5e2c92f16fef65d276ec25de3420010937f316
Ben Potter <me@bpmct.net>
2021-09-05 16:33:27 -0500

    add sessions and brief landing page
    Co-authored-by: Victoria <victoriaslocum752@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require("express");
+var cookieSession = require("cookie-session");
+
 const fetch = require("node-fetch");
 
 // our module
-const moduleToFetch = require("./index");
+const moduleToFetch = require("./getNotion");
+
+// const database = require("./getFirebase");
+var admin = require("firebase-admin");
+require("dotenv").config();
+
+// grab the firebase credentials from an environment variable
+// the environment variable is a base64 encoded ServiceAccount object
+let theCreds = admin.credential.cert(
+  JSON.parse(
+    Buffer.from(process.env.FIREBASE_CONFIG, "base64").toString("utf-8")
+  )
+);
+
+admin.initializeApp({
+  credential: theCreds,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+let database = admin.database();
 
 // user module (get token and data)
 
 // our function
 
 const nunjucks = require("nunjucks");
-const { restart } = require("nodemon");
 
 const getDatabase = moduleToFetch.getDatabase;
 
@@ -28,6 +48,17 @@ const app = express();
 
 app.use(express.static("public"));
 
+// necessary to set session cookies
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["token"],
+
+    // Cookie Options
+    maxAge: 720 * 60 * 60 * 1000, // 1 month
+  })
+);
+
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
@@ -38,7 +69,17 @@ app.get("/users", async (req, res) => {
   res.json(users);
 });
 
+app.get("/", async (req, res) => {
+  res.render("pages/landing.nj", { foo: "bar", notion_link });
+});
+
 app.get("/auth", async (req, res) => {
+  if (req.session.access_token) {
+    // if the user is already logged in
+    res.redirect("/");
+    return;
+  }
+
   // generate the "log in with Notion link"
   // docs: https://developers.notion.com/docs/authorization#prompting-users-to-add-an-integration
 
@@ -53,7 +94,7 @@ app.get("/auth_callback", async (req, res) => {
 
   const authorization = `${process.env.NOTION_CLIENT_ID}:${process.env.NOTION_CLIENT_SECRET}`;
 
-  // base64 encode it lol
+  // // base64 encode it lol
   const auth_base64 = Buffer.from(authorization).toString("base64");
 
   const access_token = await fetch("https://api.notion.com/v1/oauth/token", {
@@ -70,15 +111,30 @@ app.get("/auth_callback", async (req, res) => {
     }),
   });
 
-  console.log("what happened is", access_token);
+  let userData = await access_token.json();
 
-  let victoriaIsProbablyRighht = await access_token.json();
+  // add this user to database, if it doesn't exist
+  try {
+    // look up the user based on their access_token
+    const ref = database.ref(`/users/${userData.access_token}`);
+    const response = await ref.once("value");
+    const user = response.val();
 
-  console.log(victoriaIsProbablyRighht);
+    if (!user) {
+      // add the user to the database
+      await ref.set({ notionData: userData, dashboardPages: [] });
+    }
 
-  console.log(req.query.code);
+    // set a cookie with their access token so they don't need to log in again
+    req.session.access_token = userData.access_token;
+  } catch (err) {
+    console.error(err);
+    res.send("An error occurred", err);
+    return;
+  }
 
-  res.send(req.query);
+  // take them to the settings page
+  res.redirect("/settings");
 });
 
 app.listen(port, console.log(`Server started on ${port}`));
